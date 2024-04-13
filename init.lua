b418c63c1fd39318fd75207b163b48d3d0127dec
Ary <ary@linuxlaptop.lan>
2024-04-13 10:55:35 -0400

    Comment out overwritten command

--- init.lua
+++ init.lua
@@ -582,7 +582,7 @@ vim.keymap.set("n", "<C-g>", function() harpoon.ui:toggle_quick_menu(harpoon:lis
 vim.keymap.set("n", "<C-q>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-w>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-e>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-r>", function() harpoon:list():select(4) end)
+-- vim.keymap.set("n", "<C-r>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-n>", function() harpoon:list():prev() end)
---
+++ lua/snippets/javascript.lua
@@ -0,0 +1,14 @@
+-- File: ~/.config/nvim/lua/snippets/javascript.lua
+local luasnip = require('luasnip')
+
+luasnip.snippets.javascript = {
+  s("jsdoc", {
+    i {
+      "/**",
+      " * ${1:Description}",
+      " * @param {${2:type}} ${3:name} - ${4:Description of parameter}",
+      " * @return {${5:type}} ${6:Description of return value}",
+      " */"
+    },
+  })
+}
