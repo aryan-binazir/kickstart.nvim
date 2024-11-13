2a37c3f50fdc062e9089ca1af77972e6b7c0d97b
Aryan Binazir <abinazir@gmail.com>
2024-11-13 16:46:18 -0500

    Fix Harpoon

--- init.lua
+++ init.lua
@@ -666,36 +666,35 @@ require 'treesitter-context'.setup {
 
 -- Configure Harpoon
 local harpoon = require('harpoon')
-harpoon:setup({})
+harpoon:setup()
 
 -- basic telescope configuration
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-  local file_paths = {}
-  for _, item in ipairs(harpoon_files.items) do
-    table.insert(file_paths, item.value)
-  end
-
-  require("telescope.pickers").new({}, {
-    prompt_title = "Harpoon",
-    finder = require("telescope.finders").new_table({
-      results = file_paths,
-    }),
-    previewer = conf.file_previewer({}),
-    sorter = conf.generic_sorter({}),
-  }):find()
-end
+-- local conf = require("telescope.config").values
+-- local function toggle_telescope(harpoon_files)
+--   local file_paths = {}
+--   for _, item in ipairs(harpoon_files.items) do
+--     table.insert(file_paths, item.value)
+--   end
+--
+--   require("telescope.pickers").new({}, {
+--     prompt_title = "Harpoon",
+--     finder = require("telescope.finders").new_table({
+--       results = file_paths,
+--     }),
+--     previewer = conf.file_previewer({}),
+--     sorter = conf.generic_sorter({}),
+--   }):find()
+-- end
 
 -- vim.keymap.set("n", "<C-t>", function() toggle_telescope(harpoon:list()) end,
 --   { desc = "Open harpoon window" })
 vim.keymap.set("n", "<leader>m", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-g>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<C-5>", function() harpoon:list():select(5) end)
+vim.keymap.set("n", "<C-q>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-w>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-a>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-n>", function() harpoon:list():prev() end)
@@ -808,7 +807,7 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
-  ensure_installed = { "lua_ls", "gopls", "pyright", "eslint", "jsonls" },
+  ensure_installed = {} -- { "lua_ls", "gopls", "pyright", "eslint", "jsonls" },
 }
 
 mason_lspconfig.setup_handlers {
