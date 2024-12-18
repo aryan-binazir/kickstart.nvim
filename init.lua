1b108f718279a70eb17a2d399473106892ad333c
Aryan Binazir <abinazir@gmail.com>
2024-12-18 07:09:27 -0500

    Updates to tree

--- init.lua
+++ init.lua
@@ -786,45 +786,36 @@ cmp.setup {
     { name = 'luasnip' },
     { name = 'path' },
   },
-  require("nvim-tree").setup({
-    sort = {
-      sorter = "case_sensitive",
-    },
-    view = {
-      width = 50,
-    },
-    renderer = {
-      group_empty = true,
-      icons = {
-        glyphs = {
-          default = '',
-          symlink = '',
-          folder = {
-            arrow_closed = '',
-            arrow_open = '',
-            default = '',
-            open = '',
-            empty = '',
-            empty_open = '',
-            symlink = '',
-            symlink_open = '',
-          },
-          git = {
-            unstaged = '',
-            staged = '',
-            unmerged = '',
-            renamed = '',
-            untracked = '',
-            deleted = '',
-            ignored = '',
-          },
-        },
-      },
-    },
-    filters = {
-      dotfiles = false,
-    },
-  })
 }
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+end
+
+require("nvim-tree").setup({
+  on_attach = my_on_attach,
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = false,
+  },
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
