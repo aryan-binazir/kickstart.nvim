8f6c18548f356434e102663b0f670a0629861745
Aryan Binazir <abinazir@gmail.com>
2024-12-19 18:32:29 -0500

    Fix oil.nvim config

--- init.lua
+++ init.lua
@@ -631,7 +631,20 @@ require 'treesitter-context'.setup {
   on_attach = nil,
 }
 
-require("oil").setup()
+require("oil").setup({
+  columns = { "icons" },
+  keymap = {
+    ["<C-h>"] = false,
+    ["<C-p>"] = false,
+    ["<m-h>"] = "actions.select_split",
+  },
+  view_options = {
+    show_hidden = true
+  },
+  keymaps = {
+    ["<C-p>"] = false, -- Making sure it's disabled in both places
+  },
+})
 
 -- Configure Harpoon
 local harpoon = require("harpoon")
